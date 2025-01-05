bf53aea408f1714a421e8d35637584fbdca34e12
Matteo Migliarese <migliaresematteo@gmail.com>
2025-01-05 18:36:21 +0100

    fix: update build configuration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/main.scss";
+        `
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
