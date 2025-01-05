39ad367bc34219ceb2f805bec60b358cd628d995
Matteo Migliarese <migliaresematteo@gmail.com>
2025-01-05 18:40:27 +0100

    update index.html update netlify.toml update package-lock.json update package.json add src/styles/variables.scss update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@i18n': path.resolve(__dirname, './src/i18n')
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @import "./src/styles/main.scss";
-        `
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+        additionalData: `@import "@styles/variables.scss";`
       }
     }
   }
